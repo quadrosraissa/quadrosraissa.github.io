779c5cecb919d671da883b2bf5f5650b100b47c0
quadrosraissa <quadros.raissa@gmail.com>
2025-01-28 12:24:38 -0300

    Initial script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const images = document.querySelectorAll('.hero-images img');
+let currentIndex = 0;
+
+function rotateImages() {
+  images[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + 1) % images.length;
+  images[currentIndex].classList.add('active');
+}
+
+setInterval(rotateImages, 5000); // Change image every 5 seconds
